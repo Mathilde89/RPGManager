0b7cc6b06a9f1d2a1af17b8ad56f6b047cae139b
Mathilde Gamore <malega89@hotmail.com>
2022-11-30 09:25:38 +0100

    UP

--- resources/views/groupe/show.blade.php
+++ resources/views/groupe/show.blade.php
@@ -8,18 +8,20 @@
     <h2>Nom du groupe : {{ $value['name'] }}</h2>
     <li>Description : {{ $value['detail'] }}</li>
     <li>Nombre de places : {{ $value['nbplace'] }}</li>
-    
+
+    {{-- Pour supprimer le groupe --}}
     <form action=" {{ route('groupe.destroy' , $value['id'])}}" method="post">
         @csrf
         @method('delete')
         <input type="submit" value="Supprimer le groupe" name="submitdeletegroupe" id="">
     </form>
 
+    {{-- Pour modifier le groupe --}}
     <form action="{{ route('groupe.edit' , $value['id'])}} " method="get">
-        {{-- {{ route('groupe.delete' , $value['id'])}} --}}
         @csrf
         <input type="submit" value="Modifier le groupe" name="submitmodifygroupe" id="">
     </form>
+
     <br>
 
 @endforeach
